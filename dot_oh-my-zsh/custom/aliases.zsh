f5a19ed149994ea5993f514e24f4568e7b7d6373
Jakob Friedrich <jatfr05@gmail.com>
2025-08-27 12:03:10 +0200

    update dotfiles

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,4 @@
 # For a full list of active aliases, run `alias`.
-
+alias vim="nvim"
 alias zshconfig="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -4,3 +4,6 @@ export PATH="$HOME/bin:$PATH"
 # atuin
 . "$HOME/.atuin/bin/env"
 eval "$(atuin init zsh)"
+
+# pyenv
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
