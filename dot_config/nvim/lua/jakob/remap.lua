fcf61083e309839fb588d7ca9945ac515ca82bfb
Jakob Friedrich <jatfr05@gmail.com>
2025-09-11 21:28:19 +0200

    update nvim config

--- dot_config/nvim/lua/jakob/remap.lua
+++ dot_config/nvim/lua/jakob/remap.lua
@@ -22,6 +22,5 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
