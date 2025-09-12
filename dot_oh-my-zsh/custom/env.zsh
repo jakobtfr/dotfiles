04bf1896102916b1b8bcc11e16f339ed954a97b6
Jakob Friedrich <jakob.friedrich@sz.de>
2025-09-12 11:00:57 +0200

    add work specific configs

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -8,3 +8,14 @@ eval "$(atuin init zsh)"
 
 # pyenv
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+# RBENV
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - zsh)"
+
+# ANDROID SDK
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+
+# JAVA
+export JAVA_HOME=$(/usr/libexec/java_home)
