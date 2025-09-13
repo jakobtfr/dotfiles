49c9264df256b6f9f3dd20c595539dd346268332
Jakob Friedrich <jatfr05@gmail.com>
2025-09-13 11:51:27 +0200

    add workspecific configs
    update
    
    add tmux plugins
    
    remove unneeded nvim plugins
    
    update readme
    
    remove unneeded ohmyzsh custom files
    
    add p10k

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # dotfiles
+Managed by Chezmoi:
+- zshrc
+- ohmyzsh
+  - /custom/aliases.zsh
+  - /custom/env.zsh
+- nvim
+- tmux
+- aerospace
+
---
+++ dot_config/nvim/lua/jakob/plugins/markview.lua
@@ -0,0 +1,11 @@
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    preview = "devicons",
+    -- For `nvim-treesitter` users.
+    priority = 49,
+
+    dependencies = {
+        "saghen/blink.cmp"
+    },
+};
