7faf3739b5d5b6eb4a1ca1b727cf2dea70c2dd7c
Jakob Friedrich <jatfr05@gmail.com>
2025-09-23 09:47:09 +0200

    add nvimtree cd remap

--- dot_config/nvim/lua/jakob/plugins/nvimtree.lua
+++ dot_config/nvim/lua/jakob/plugins/nvimtree.lua
@@ -6,6 +6,19 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- Default mappings
+				api.config.mappings.default_on_attach(bufnr)
+				-- Custom mappings
+				vim.keymap.set("n", "<C-r>", api.tree.change_root_to_node, opts("CD"))
+			end,
+		})
 	end,
 }
