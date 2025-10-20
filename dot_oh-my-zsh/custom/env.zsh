a5c3434fba882509694558c7cf84586d8ff11d56
Jakob Friedrich <jatfr05@gmail.com>
2025-10-20 17:49:18 +0200

    remove xelatex

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -2,8 +2,6 @@ export EDITOR="nvim"
 export PATH="$HOME/bin:$PATH"
 export DISABLE_AUTO_TITLE='true'
 
-export PATH="/Library/TeX/texbin:$PATH"
-
 # atuin
 . "$HOME/.atuin/bin/env"
 eval "$(atuin init zsh)"
