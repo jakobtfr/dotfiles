a60aed7b7eaad07a195201b6b1939016408295b3
Jakob Friedrich <jatfr05@gmail.com>
2025-10-21 15:29:01 +0200

    move to starship

--- dot_config/shell/env.zsh
+++ dot_config/shell/env.zsh
@@ -2,9 +2,8 @@ export EDITOR="nvim"
 export PATH="$HOME/bin:$PATH"
 export DISABLE_AUTO_TITLE='true'
 
+export PATH="/Library/TeX/texbin:$PATH"
+
 # atuin
 . "$HOME/.atuin/bin/env"
 eval "$(atuin init zsh)"
-
-# pyenv
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
