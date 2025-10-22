556302082fefea8164ea8d05eac4d67fdc250e00
Jakob Friedrich <jatfr05@gmail.com>
2025-10-22 10:53:38 +0200

    add wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	color_scheme = "Tokyo Night",
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	send_composed_key_when_left_alt_is_pressed = true,
+	send_composed_key_when_right_alt_is_pressed = false,
+	font_size = 15.0,
+}
+return config
