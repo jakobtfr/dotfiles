0b16d8d3b068ab65a8e30c251cf65cabfab7d04a
Jakob Friedrich <jatfr05@gmail.com>
2025-11-04 10:35:26 +0100

    add JAVA_HOME

--- dot_config/shell/env.zsh
+++ dot_config/shell/env.zsh
@@ -3,6 +3,8 @@ export PATH="$HOME/bin:$PATH"
 export DISABLE_AUTO_TITLE='true'
 
 export PATH="/Library/TeX/texbin:$PATH"
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH="$JAVA_HOME/bin:$PATH"
 
 # atuin
 . "$HOME/.atuin/bin/env"
