bb25992520b1a38d83ac3c5b44344c89abe26e16
Jakob Friedrich <jatfr05@gmail.com>
2025-12-03 11:41:36 +0100

    add llvm to path

--- dot_config/shell/env.zsh
+++ dot_config/shell/env.zsh
@@ -5,6 +5,7 @@ export DISABLE_AUTO_TITLE='true'
 export PATH="/Library/TeX/texbin:$PATH"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH="$JAVA_HOME/bin:$PATH"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 
 # atuin
 . "$HOME/.atuin/bin/env"
