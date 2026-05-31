fc633decb8fa1cb9b16e0de7fe7c5a4214256f8c
Jakob Friedrich <jatfr05@gmail.com>
2026-05-31 09:40:38 +0200

    chore(dotfiles): updated dot_config/nvim/lua/jakob/init.lua

--- dot_config/nvim/lua/jakob/init.lua
+++ dot_config/nvim/lua/jakob/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 require("jakob.pack_init")
+require("jakob.pack_update").setup()
 require("jakob.remap")
 require("jakob.set")
 require("jakob.lsp")
--- dot_config/nvim/lua/jakob/plugins/treesitter.lua
+++ dot_config/nvim/lua/jakob/plugins/treesitter.lua
@@ -1,6 +1,35 @@
+local languages = {
+	"python",
+	"javascript",
+	"typescript",
+	"c",
+	"lua",
+	"vim",
+	"vimdoc",
+	"query",
+	"markdown",
+	"markdown_inline",
+}
+
+local function enable_highlighting()
+	local group = vim.api.nvim_create_augroup("JakobTreesitterHighlight", { clear = true })
+
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		callback = function(event)
+			local lang = vim.treesitter.language.get_lang(event.match)
+			if lang == nil or not vim.tbl_contains(languages, lang) then
+				return
+			end
+
+			pcall(vim.treesitter.start, event.buf, lang)
+		end,
+	})
+end
+
 return {
 	specs = {
-		{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
+		{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	},
 	before_add = function()
 		vim.api.nvim_create_autocmd("PackChanged", {
@@ -11,7 +40,9 @@ return {
 
 				vim.schedule(function()
 					pcall(vim.cmd.packadd, "nvim-treesitter")
-					if vim.fn.exists(":TSUpdateSync") == 2 then
+					if vim.fn.exists(":TSUpdate") == 2 then
+						pcall(vim.cmd.TSUpdate)
+					elseif vim.fn.exists(":TSUpdateSync") == 2 then
 						pcall(vim.cmd.TSUpdateSync)
 					end
 				end)
@@ -19,23 +50,25 @@ return {
 		})
 	end,
 	setup = function()
+		local treesitter = require("nvim-treesitter")
+
+		if type(treesitter.install) == "function" then
+			treesitter.setup()
+			if #vim.api.nvim_list_uis() > 0 then
+				pcall(treesitter.install, languages)
+			end
+			enable_highlighting()
+			return
+		end
+
+		-- Legacy master branch fallback. Markdown highlighting can crash on Neovim 0.12.
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"python",
-				"javascript",
-				"typescript",
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"markdown",
-				"markdown_inline",
-			},
+			ensure_installed = languages,
 			sync_install = false,
 			auto_install = true,
 			highlight = {
 				enable = true,
+				disable = { "markdown", "markdown_inline" },
 				additional_vim_regex_highlighting = false,
 			},
 		})
