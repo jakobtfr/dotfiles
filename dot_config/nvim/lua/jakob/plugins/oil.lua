41a21ff5819a1640cb687c4da2bec8ecd778f84c
Jakob Friedrich <jatfr05@gmail.com>
2026-06-04 18:19:34 +0200

    chore(dotfiles): updated dot_config/nvim/lua/jakob/plugins/oil.lua

--- dot_config/nvim/lua/jakob/plugins/oil.lua
+++ dot_config/nvim/lua/jakob/plugins/oil.lua
@@ -1,11 +1,11 @@
 return {
 	specs = {
-		{ src = "https://github.com/nvim-mini/mini.icons" },
+		{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 		{ src = "https://github.com/stevearc/oil.nvim" },
 	},
 	setup = function()
-		require("mini.icons").setup()
+		require("nvim-web-devicons").setup()
 		require("oil").setup()
-		vim.keymap.set("n", "<leader><space>", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
