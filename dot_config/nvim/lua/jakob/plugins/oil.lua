9c5962901161f7537a6993eb93567aeee7715db6
Jakob Friedrich <jatfr05@gmail.com>
2026-06-04 18:16:52 +0200

    chore(dotfiles): updated dot_config/nvim/lua/jakob/plugins/oil.lua

--- dot_config/nvim/lua/jakob/plugins/oil.lua
+++ dot_config/nvim/lua/jakob/plugins/oil.lua
@@ -1,9 +1,11 @@
 return {
 	specs = {
+		{ src = "https://github.com/nvim-mini/mini.icons" },
 		{ src = "https://github.com/stevearc/oil.nvim" },
 	},
 	setup = function()
+		require("mini.icons").setup()
 		require("oil").setup()
-		vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader><space>", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
