48bed8f6cefd08ce731145c5985c9953f593b1b4
Jakob Friedrich <jatfr05@gmail.com>
2026-06-04 18:39:56 +0200

    chore(dotfiles): updated dot_config/nvim/lua/jakob/plugins/oil.lua

--- dot_config/nvim/lua/jakob/plugins/oil.lua
+++ dot_config/nvim/lua/jakob/plugins/oil.lua
@@ -5,7 +5,18 @@ return {
 	},
 	setup = function()
 		require("nvim-web-devicons").setup()
-		require("oil").setup()
+		require("oil").setup({
+			delete_to_trash = true,
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-l>"] = false,
+				["<C-t>"] = false,
+				["<C-c>"] = false,
+			},
+			float = {
+				preview_split = "right",
+			},
+		})
 		vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
