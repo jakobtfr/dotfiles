c81748157077b261d48d72424ea6c706dfbc560f
Jakob Friedrich <jakobfri@amazon.com>
2026-06-08 11:22:16 +0200

    chore(dotfiles): updated dot_config/nvim/lua/jakob/autoreload.lua

--- dot_config/nvim/lua/jakob/autoreload.lua
+++ dot_config/nvim/lua/jakob/autoreload.lua
@@ -1,10 +1,23 @@
+vim.opt.autoread = true
+
 vim.api.nvim_create_augroup("AutoReloadFile", { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "CursorHold", "CursorHoldI" }, {
 	group = "AutoReloadFile",
 	pattern = "*",
 	callback = function()
-		vim.cmd("checktime")
+		if vim.fn.mode() ~= "c" and vim.fn.getcmdwintype() == "" then
+			vim.cmd("checktime")
+		end
 	end,
-	desc = "Check for file changes when entering buffer or gaining focus",
+	desc = "Check for external file changes on idle, buffer enter, or focus",
+})
+
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+	group = "AutoReloadFile",
+	pattern = "*",
+	callback = function()
+		vim.notify("File changed on disk, buffer reloaded", vim.log.levels.INFO)
+	end,
+	desc = "Notify when a buffer is reloaded after an external change",
 })
