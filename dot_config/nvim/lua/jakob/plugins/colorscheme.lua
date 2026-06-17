ea8fab8349028fb83edb83c7ebae1e7d5ffe21a0
Jakob Friedrich <jakobfri@amazon.com>
2026-06-17 14:25:01 +0200

    chore(dotfiles): updated dot_config/nvim/lua/jakob/plugins/colorscheme.lua

--- dot_config/nvim/lua/jakob/plugins/colorscheme.lua
+++ dot_config/nvim/lua/jakob/plugins/colorscheme.lua
@@ -3,6 +3,9 @@ return {
 		{ src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
 	},
 	setup = function()
+		require("rose-pine").setup({
+			styles = { transparency = true },
+		})
 		vim.cmd.colorscheme("rose-pine")
 	end,
 }
