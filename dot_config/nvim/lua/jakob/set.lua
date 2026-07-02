5a36fb5c2bd3cdd0289d0a105bb7b092c0c72252
Jakob Friedrich <jakobfri@amazon.com>
2026-07-02 14:06:30 +0000

    chore(dotfiles): updated dot_config/nvim/lua/jakob/set.lua

--- dot_config/nvim/lua/jakob/set.lua
+++ dot_config/nvim/lua/jakob/set.lua
@@ -7,9 +7,20 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.opt.wrap = true
-vim.opt.linebreak = true -- wrap at word boundaries, not mid-word
+vim.opt.wrap = false -- default off (code); prose filetypes turn it on below
+vim.opt.linebreak = true -- when wrap is on, break at word boundaries, not mid-word
 vim.opt.breakindent = true -- wrapped lines keep the line's indent
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "text", "gitcommit", "tex", "typst", "help" },
+	callback = function()
+		vim.opt_local.wrap = true
+	end,
+})
+
+-- move by visual line unless a count is given, so wrapped prose scrolls row-by-row
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
 vim.opt.swapfile = false
 vim.opt.undofile = true
